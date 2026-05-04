a8cb29aa3bc0cdb28275f6d2c0dc746ad27b86b5
김하림 <hrkim0529@naver.com>
2026-05-04 18:35:30 +0900

    chore: 중복 MissionException 파일 삭제

--- src/main/java/com/example/umc10th/domain/mission/exception/MisisonException.java
+++
@@ -1,7 +0,0 @@
-package com.example.umc10th.domain.mission.exception;
-
-public class MisisonException extends RuntimeException {
-    public MisisonException(String message) {
-        super(message);
-    }
-}
